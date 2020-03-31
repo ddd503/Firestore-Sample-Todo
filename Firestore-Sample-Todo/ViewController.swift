86df5ecce40e88fdca9e86e28ea337762c3d1588
Dai Kawahara <skbsporta7@gmail.com>
2020-03-31 20:09:32 +0900

    Initial commit

---
+++ Firestore-Sample-Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firestore-Sample-Todo
+//
+//  Created by kawaharadai on 2020/03/31.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
