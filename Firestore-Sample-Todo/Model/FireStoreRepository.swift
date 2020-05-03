567c64ab22a6a747d8825438244e8a46bd79ed8e
Dai Kawahara <skbsporta7@gmail.com>
2020-05-03 23:46:24 +0900

    add top viewcontroller

---
+++ Firestore-Sample-Todo/Model/FireStoreRepository.swift
@@ -0,0 +1,19 @@
+//
+//  FireStoreRepository.swift
+//  Firestore-Sample-Todo
+//
+//  Created by kawaharadai on 2020/05/03.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import Foundation
+
+protocol FireStoreRepository {
+    func readAll()
+}
+
+struct FireStoreRepositoryImpl: FireStoreRepository {
+    func readAll() {
+
+    }
+}
---
+++ Firestore-Sample-Todo/View/TodoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListViewController.swift
+//  Firestore-Sample-Todo
+//
+//  Created by kawaharadai on 2020/05/03.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+final class TodoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
